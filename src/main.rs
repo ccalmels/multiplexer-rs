26952ba9466dc65e9c356dc08a26660e29ef235f
Clement Calmels <clement.calmels@free.fr>
2020-02-12 13:54:26 +0100

    read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    println!("You typed: {}", input.trim());
 }
