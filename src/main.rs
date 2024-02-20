96ce9189f563b982ec02a891cb761315b037536b
Clement Calmels <clement.calmels@free.fr>
2024-02-20 11:18:22 +0100

    use /// for help in clap Args structure

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ use multiplexer_rs::run;
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    /// listening socket address
     #[arg(
         short,
         long,
         default_value = "localhost:1234",
         id = "ADDRESS:PORT",
-        help = "listening socket addresse"
     )]
     listen: String,
 
-    #[arg(short, long, help = "make blocking writes")]
+    /// make blocking writes
+    #[arg(short, long)]
     block: bool,
-    #[arg(short, long, help = "make parallel writing to client")]
+    /// make parallel writing to client
+    #[arg(short, long)]
     parallel: bool,
+    /// command to execute
     #[arg(trailing_var_arg = true)]
     cmd: Vec<String>,
 }
